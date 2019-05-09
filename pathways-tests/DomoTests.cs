de02e57ed221f31ef47ac1e32c15adb57d11ba43
Sarah Fernandez <sarah.fernandez1@wipro.com>
2019-05-09 16:18:23 -0400

    Created pathways-tests solution and set up xunit with an empty DomoTests

---
+++ pathways-tests/DomoTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace pathways_tests
+{
+    public class DomoTests
+    {
+        //tests the domo third-party integration
+        [Fact]
+        public void ShouldAccessDomo()
+        {
+
+        }
+    }
+}
