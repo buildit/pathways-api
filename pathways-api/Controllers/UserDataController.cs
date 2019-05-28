3d6cf88d2947732ec6303dd70e2ca761cee5c9a0
Sarah Fernandez <sarah.fernandez1@wipro.com>
2019-05-28 18:29:58 -0400

    domo auth working correctly, access token returned

--- pathways-api/Controllers/UserDataController.cs
+++ pathways-api/Controllers/UserDataController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using pathways_api.Data.Mappers;
 
@@ -12,16 +15,16 @@ namespace pathways_api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserDataController : ControllerBase//PathwaysController
+    public class UserDataController : ControllerBase //PathwaysController
     {
         //Get bench info from Domo
 
         private readonly DomoClient _domoClient;
 
-        public UserDataController()//DomoSettings domoSettings)//IOptions<DomoSettings> domoSettingsAccessor)
+        public UserDataController() //DomoSettings domoSettings)//IOptions<DomoSettings> domoSettingsAccessor)
         {
-           // _domoSettings = domoSettingsAccessor.Value;
-           _domoClient = new DomoClient();
+            // _domoSettings = domoSettingsAccessor.Value;
+            _domoClient = new DomoClient();
         }
 
         // GET api/UserData
@@ -31,84 +34,61 @@ namespace pathways_api.Controllers
 
             //get access token 
             //move to service seperatly reusable
-            
+
             var domoClient = new HttpClient();
-            var request = new HttpRequestMessage() {
+            var request = new HttpRequestMessage()
+            {
                 RequestUri = new Uri("https://api.domo.com/oauth/token?grant_type=client_credentials&scope=data"),
                 Method = HttpMethod.Get,
             };
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsonObject"));
-            //request.Headers.Add("clientId", "9a224ad1-38e1-48c0-af0f-2c3be9f5c6d8");//(_domoClient.ClientId));
-            //request.Headers.Add("secret", "53d5d3c25e9f0cf7e3ff947d709f6e05cbfcdf9eb659ab8cc81f40522d3306a5");//(_domoClient.ClientId));
-            //request.Headers.Add("Content-Type", "application/jsonObject");
-            var AuthorizationString = "9a224ad1-38e1-48c0-af0f-2c3be9f5c6d8" + ":" +
-                                      "53d5d3c25e9f0cf7e3ff947d709f6e05cbfcdf9eb659ab8cc81f40522d3306a5";
+            var AuthorizationString = "b8ac9139-d8f6-4bff-b7c8-34167d242ee6" + ":" +
+                                      "73f47d8f10cfb960aa3ac3b2c6647ceeccf2788c6b13259de018e74dd6abe617";
             var encodedAuthorizationString = Base64Encode(AuthorizationString);
+            request.Headers.Add("Accept", "application/json");
             request.Headers.Add("Authorization", "Basic " + encodedAuthorizationString);
 
+            var domoResponse = await domoClient.SendAsync(request).ConfigureAwait(false);
 
+            domoResponse.EnsureSuccessStatusCode();
+            var aouthJson = await domoResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
 
+            var aouthObject = JObject.Parse(aouthJson);
 
-            var localStorage = "";
-            
-            var domoResponse = await domoClient.PostAsync(request.RequestUri, new FormUrlEncodedContent(parameters));
-            var contents = await response.Content.ReadAsStringAsync();
-            
-            
-            
-            var task = domoClient.SendAsync(request)
-                .ContinueWith((taskwithmsg) =>
-                {
-                    var response = taskwithmsg.Result;
-                    localStorage= "Bearer " + response.Content.Headers.
-                                      
-                    var jsonTask = response.Content.ReadAsAsync<JsonObjectContract>();
-                    jsonTask.Wait();
-                    var jsonObject = jsonTask.Result;
-                });
-            //task.Wait();
+            var domoKeyValue = aouthObject.Descendants().OfType<JProperty>().Where(p => p.Name == "access_token")
+                .Select(x => x.Value.ToString()).ToArray();
 
 
             //access userData
             var userdata = new UserDataRequestDto();
 
-            try
+            var userDataClient = new HttpClient();
+            var userDataRequest = new HttpRequestMessage()
             {
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri("https://api.domo.com/v1/datasets/8b209fa4-bb92-4b39-8e73-3f4183542129");
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                RequestUri = new Uri("https://api.domo.com/v1/datasets"),//"/8b209fa4-bb92-4b39-8e73-3f4183542129"),
+                Method = HttpMethod.Get,
+            };
 
-                    var response = await client.GetAsync(_domoClient.DataSetsUrl + _domoClient.BenchDataSetId)
-                        .ConfigureAwait(false);
+            userDataRequest.Headers.Add("Accept", "application/json");
+            userDataRequest.Headers.Add("Authorization", "bearer " + domoKeyValue.First());
+            
+            var userDataResponse = await userDataClient.SendAsync(userDataRequest).ConfigureAwait(false);
 
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // userdata = map response to userdatarequestdto
-                        var data = await response.Content.ReadAsAsync<UserDataRequestDto>(); //getting output
+                // userdata = map response to userdatarequestdto
+                //var data = await userDataResponse.Content.ReadAsAsync<UserDataRequestDto>(); //getting output
+                var userDataJson = await userDataResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var userDataObject = JObject.Parse(userDataJson);
 
-                    }
-
-                    return userdata;
-                }
-            }
-
-            catch (Exception e)
-            {
-                return userdata;
-            }
+            return userdata;
         }
+    
         
+        //move to service
         public static string Base64Encode(string plainText) {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
-
-// programs requesting access tokens has to know the client secret in order to gain the token
-//client secret authorizes an app to request access tokens, it doesnt authenticate the user
 
 //query examples
 //.get('/data/v1/sales?fields=rep,amount&groupby=rep') --groupby rep
