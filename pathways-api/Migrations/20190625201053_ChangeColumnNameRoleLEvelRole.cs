944e3d335d1add708b39d578e13442b37818949e
Sarah Fernandez <sarah.fernandez1@wipro.com>
2019-06-25 16:11:33 -0400

    Migration to change column name on RoleLevelRole

---
+++ pathways-api/Migrations/20190625201053_ChangeColumnNameRoleLEvelRole.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pathways_api.Migrations
+{
+    public partial class ChangeColumnNameRoleLEvelRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_rolelevelrule_rolelevel_SkillLevelId",
+                schema: "admin",
+                table: "rolelevelrule");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_rolelevelrule_skilllevel_SkillLevelId",
+                schema: "admin",
+                table: "rolelevelrule",
+                column: "SkillLevelId",
+                principalSchema: "skills",
+                principalTable: "skilllevel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_rolelevelrule_skilllevel_SkillLevelId",
+                schema: "admin",
+                table: "rolelevelrule");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_rolelevelrule_rolelevel_SkillLevelId",
+                schema: "admin",
+                table: "rolelevelrule",
+                column: "SkillLevelId",
+                principalSchema: "skills",
+                principalTable: "rolelevel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
